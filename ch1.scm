f2c0a543035315a543909497013241720399da82
Mack Howell <mhowell@mhowel-ny-06112.ad.hugeinc.com>
2015-02-04 16:51:48 -0500

    1.8

--- ch1.scm
+++ ch1.scm
@@ -155,7 +155,7 @@
 ;   (/ (+ x y) 2))
 
 ; (define (good-enough? guess x)
-;   (< (abs (- (square guess) x)) guess) (* guess .0001))
+;   (< (abs (- (square guess) x) guess) (* guess .0001)))
 
 ; (define (sqrt x)
 ;   (sqrt-iter 1.0 x))
@@ -167,3 +167,26 @@
 ; this puts things in terms of absoulte value (instead of % or an relative tolerance)
 
 
+
+; -- 1.8 --
+(define (cube-rt-iter guess previous-guess x)
+  (if (good-enough? guess previous-guess)
+    guess
+    (cube-rt-iter (improve guess x)
+                  guess
+                  x)))
+
+(define (improve guess x)
+  (/ (+ (/ x
+           (square guess))
+        (* 2 guess))
+     3))
+
+(define (good-enough? guess previous-guess)
+  (< (abs (- guess previous-guess))
+     .0001))
+
+(define (cube-rt x)
+  (cube-rt-iter 1.0 0 x))
+
+(cube-rt 9)
